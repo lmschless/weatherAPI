15997e383f4bb7765239a39796aa45d820fba59c
Luke <lmschless@gmail.com>
2020-03-10 19:18:33 -0700

    first upload to repo

---
+++ src/weather.js
@@ -0,0 +1,36 @@
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './gameUI.js';
+// import $ from 'jquery';
+
+$(document).ready(function() {
+	$('#weatherLocation').click(function() {
+		const city = $('#location').val();
+		$('#location').val('');
+
+		let promise = new Promise(function(resolve, reject) {
+			let request = new XMLHttpRequest();
+			const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+			request.onload = function() {
+				if (this.status === 200) {
+					resolve(request.response);
+				} else {
+					reject(Error(request.statusText));
+				}
+			};
+			request.open('GET', url, true);
+			request.send();
+		});
+
+		promise.then(
+			function(response) {
+				let body = JSON.parse(response);
+				$('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
+				$('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+			},
+			function(error) {
+				$('.showErrors').text(`There was an error processing your request: ${error.message}`);
+			}
+		);
+	});
+});
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+var webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+	entry: './src/weather.js', // './src/index': './src/main.js'
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devtool: 'eval-source-map',
+	devServer: {
+		contentBase: './dist'
+	},
+	plugins: [
+		new UglifyJsPlugin({ sourceMap: true }),
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'Did You Get Eaten',
+			template: './src/index.html',
+			inject: 'head'
+		}),
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery'
+		}),
+		new Dotenv()
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [ 'style-loader', 'css-loader' ]
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_module/,
+				loader: 'eslint-loader'
+			}
+		]
+	}
+};
